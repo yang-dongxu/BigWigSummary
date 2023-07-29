aa58ae4b6f1e87c331ce791c31594425597a0fcd
Documenter.jl <documenter@juliadocs.github.io>
2023-07-29 13:15:09 +0000

    build based on b1833f3

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = BigWigSummary","category":"page"},{"location":"#BigWigSummary","page":"Home","title":"BigWigSummary","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for BigWigSummary.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [BigWigSummary]","category":"page"}]
+}
